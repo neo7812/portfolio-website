1e9af34ba4794cca071e87a5f68e83e18c522e69
Saurabh Upadhyay <73853612+neo7812@users.noreply.github.com>
2025-03-24 18:27:02 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -380,7 +380,7 @@ export default function Home() {
                 style={{ width: '100%', height: '100%' }}
               />
             </a>
-            { <a href="globetrotter-flax.vercel.app" className="bento-item">
+            <a href="globetrotter-flax.vercel.app" className="bento-item">
               <Image
                 src="/imgs/bento-2.png"
                 alt="Churhview"
@@ -415,7 +415,7 @@ export default function Home() {
                 height={300}
                 style={{ width: '100%', height: '100%' }}
               />
-            </a> }
+            </a> 
             <a
               href="https://saas-landing-page-wine.vercel.app/"
               className="bento-item"
