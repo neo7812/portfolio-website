8815309077f2b2858dc7661ea072ba62edec2649
Saurabh Upadhyay <73853612+neo7812@users.noreply.github.com>
2025-03-24 18:30:28 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -380,7 +380,7 @@ export default function Home() {
                 style={{ width: '100%', height: '100%' }}
               />
             </a>
-            <a href="globetrotter-flax.vercel.app" className="bento-item">
+            <a href="https://globetrotter-flax.vercel.app/" className="bento-item">
               <Image
                 src="/imgs/bento-2.png"
                 alt="Churhview"
@@ -389,33 +389,33 @@ export default function Home() {
                 style={{ width: '100%', height: '100%' }}
               />
             </a>
-            <a href="#" className="bento-item">
-              <Image
-                src="/imgs/bento-3.jpg"
-                alt="Harley"
-                width={400}
-                height={300}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </a>
-            <a href="#" className="bento-item">
-              <Image
-                src="/imgs/bento-5.jpg"
-                alt="Bunbury"
-                width={400}
-                height={300}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </a>
-            <a href="#" className="bento-item">
-              <Image
-                src="/imgs/bento-6.jpg"
-                alt="Running"
-                width={400}
-                height={300}
-                style={{ width: '100%', height: '100%' }}
-              />
-            </a> 
+            // <a href="#" className="bento-item">
+            //   <Image
+            //     src="/imgs/bento-3.jpg"
+            //     alt="Harley"
+            //     width={400}
+            //     height={300}
+            //     style={{ width: '100%', height: '100%' }}
+            //   />
+            // </a>
+            // <a href="#" className="bento-item">
+            //   <Image
+            //     src="/imgs/bento-5.jpg"
+            //     alt="Bunbury"
+            //     width={400}
+            //     height={300}
+            //     style={{ width: '100%', height: '100%' }}
+            //   />
+            // </a>
+            // <a href="#" className="bento-item">
+            //   <Image
+            //     src="/imgs/bento-6.jpg"
+            //     alt="Running"
+            //     width={400}
+            //     height={300}
+            //     style={{ width: '100%', height: '100%' }}
+            //   />
+            // </a> 
             <a
               href="https://saas-landing-page-wine.vercel.app/"
               className="bento-item"
